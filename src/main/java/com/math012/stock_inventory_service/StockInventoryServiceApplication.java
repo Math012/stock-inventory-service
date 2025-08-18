e315664392837427c5c63ce6f109946c27d93fff
Math012 <math111.0811@gmail.com>
2025-08-18 18:36:05 -0300

    Primeiro commit

---
+++ src/main/java/com/math012/stock_inventory_service/StockInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.math012.stock_inventory_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockInventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockInventoryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/math012/stock_inventory_service/StockInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.math012.stock_inventory_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockInventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
